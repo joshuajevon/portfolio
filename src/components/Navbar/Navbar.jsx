6031ab8a07914a41c593cc634658f50ccad2c8e8
joshuajevon <joshuajevon05@gmail.com>
2023-07-21 20:58:41 +0700

    fix: navbar respinsiuve

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,51 +1,36 @@
-import React, { useState } from 'react';
+import React, { Component, useState } from 'react';
 import "../../styles/components/_navbar.scss";
 import Logo from "../../assets/icon/logo.svg";
 
-const Navbar = () => {
-    const [isNavOpen, setIsNavOpen] = useState(false);
-  
-    const handleNavToggle = () => {
-      setIsNavOpen(!isNavOpen);
+class Navbar extends Component{
+    state={clicked: false};
+
+    handleClick = () => {
+      this.setState({clicked: !this.state.clicked})
+    }
+
+    render(){
+      return (
+        <nav>
+          <a href="/">
+            <img src={Logo} alt="" />
+          </a>
+          <div className="">
+            <ul id='navbar' className={this.state.clicked ? "#navbar active" : "#navbar"}>
+              <li><a className='active' href="">Home</a></li>
+              <li><a href="">About</a></li>
+              <li><a href="">Skills</a></li>
+              <li><a href="">Projects</a></li>
+              <li><a href="">Contact</a></li>
+            </ul>
+          </div>
+
+          <div id="mobile" onClick={this.handleClick}>
+            <i id='bar' className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+          </div>
+        </nav>
+      );
     };
-  
-    return (
-      <nav className="navbar">
-        <div className="navbar-logo">
-            <a href="/">
-                <img
-                    className=""
-                    src={Logo}
-                    alt="logo"
-                />
-            </a>
-        </div>
-        <div className={`navbar-menu ${isNavOpen ? 'active' : ''}`}>
-          <ul className="navbar-list">
-            <li className="navbar-item">
-                <a href="">Home</a>
-            </li>
-            <li className="navbar-item">
-                <a href="">About</a>
-                </li>
-            <li className="navbar-item">
-                <a href="">Skills</a>
-                </li>
-            <li className="navbar-item">
-                <a href="">Projects</a>
-                </li>
-            <li className="navbar-item">
-                <a href="">Contact</a>
-                </li>
-          </ul>
-        </div>
-        <div className="navbar-toggle" onClick={handleNavToggle}>
-          <div className="bar"></div>
-          <div className="bar"></div>
-          <div className="bar"></div>
-        </div>
-      </nav>
-    );
-  };
-  
+  }
+
   export default Navbar;
